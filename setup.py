01121e6529087fac7c5f686b4b3fa67a6dde2670
ivanvig <ivanmvig@gmail.com>
2023-03-01 12:09:48 -0300

    changed module setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='headergen',
-    version='0.1.0',
+    name='ralbot',
+    version='1.0.0',
     description='Generate a C header from a SystemRDL register definition',
     url='https://github.com/Julian-DD/SystemRDL-C-header',
-    packages=find_packages(exclude=('test*')),
+    packages=find_packages(),
+    install_requires=['peakrdl']
 )

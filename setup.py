9876ca89f521b39ee6ca2de1fd2176e4719894b3
rmoral <ramiro.moral@falcoprocessing.com>
2023-04-13 13:01:39 -0300

    updating version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ralbot',
-    version='1.0.0',
+    version='1.1.0',
     description='Generate a C header from a SystemRDL register definition',
     url='https://github.com/Julian-DD/SystemRDL-C-header',
     packages=find_packages(),
